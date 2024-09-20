4b21ad70ab3b580c562c69e8a10f3b3241c3fd40
wuton <wuton@LAPTOP-G247QF38>
2024-09-20 17:27:26 +0800

    9/15日组会期间的修改

--- gra desgin/save_data.m
+++ gra desgin/save_data.m
@@ -1,2 +1,2 @@
 % save('material_0_SCH4_control_280K.mat');
-save('material_0_SCH4_v_0.00_Tin_185K_初值随机_宽松调控_entrance_6W_失败数据.mat')
+save('material_0_SCH4_v_0.014_Tin_185K_初值随机_宽松调控_grad_3.4_exit_6W.mat')
