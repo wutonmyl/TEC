10a1cfd97c3432c6b37c2d96c08de00ef3222058
wuton <wuton@LAPTOP-G247QF38>
2024-10-09 15:06:20 +0800

    10/7号组会时的一些修改

--- gra desgin/show.m
+++ gra desgin/show.m
@@ -18,7 +18,7 @@
 % Ti_record = Ti_record';
 % Tg_record = Ti_record';
 % h_record = h_record';
-data_single = data_ctl.case4_1_1.grad_5_4
+data_single = data_ctl.case1_2_1.grad_3_2197
 % data_single = data_v0_Tin_185K_P_6W_entrance.grad_5_4849;
 tiledlayout(8,3);
 % nexttile
